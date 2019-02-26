b5e75f949d4272428964330b8d95f67f28836328
Vishal V <relay.10d@gmail.com>
2019-02-26 16:16:59 +0530

    _not reachable

--- Assets/Scripts/FABRIK.cs
+++ Assets/Scripts/FABRIK.cs
@@ -25,7 +25,18 @@ public class FABRIK : MonoBehaviour
 
     private void Fabrik() 
     {
-        TargetRootDist = JointsRef.jointsData[0].JointsPos - EndEffector.position;
+        TargetRootDist = JointsRef.jointsData[0].JointsPos - GoalObject.position;
+
+        bool _checkReach = CheckForReach(TargetRootDist);
+        if(_checkReach)
+        {
+            TargetOutofReach();
+        }
+        else
+        {
+
+        }
+
 
         while( TargetRootDist.magnitude > 0.1 ) 
         {
@@ -43,5 +54,36 @@ public class FABRIK : MonoBehaviour
     private void RootToFinal()
     {
 
+    }
+
+    private bool CheckForReach(Vector3 _targetroot)
+    {   
+        float temp=0;
+        for(int i=0; i<JointsRef.jointsData.Count;i++)
+        {
+            temp += JointsRef.jointsData[i].DeltaJointPos.magnitude;
+        }
+
+        if(_targetroot.magnitude > temp)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void TargetOutofReach()
+    {
+        for(int j=0; j<JointsRef.jointsData.Count-1; j++)
+        {
+            float r_i = (GoalObject.position).magnitude - JointsRef.jointsData[j].JointsPos.magnitude;
+            float lamd_i = JointsRef.jointsData[j].DeltaJointPos.magnitude  /  r_i;
+
+            Vector3 _npos = (1-lamd_i)*JointsRef.jointsData[j].JointsPos + lamd_i*GoalObject.position;
+
+            JointsRef.jointsData[j+1].NewJointsPos = _npos;
+        }
     }
 }
--- Assets/Scripts/JointsData.cs
+++ Assets/Scripts/JointsData.cs
@@ -7,10 +7,17 @@ public class JointsData : MonoBehaviour
    public Vector3 JointsPos;
    public Vector3 DeltaJointPos;
 
+   public Vector3 NewJointsPos;
+
     public JointsData(Vector3 _jointsPos, Vector3 _deltaJointPos)
     {
         this.JointsPos = _jointsPos;
         this.DeltaJointPos = _deltaJointPos;
+    }
+
+    public JointsData(Vector3 _newjointsPos)
+    {
+        this.NewJointsPos = _newjointsPos;
     }
 
 }
